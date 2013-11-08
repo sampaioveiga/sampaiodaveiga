b9e6651e0b0bbee2b2e11949fab16c8857fa8a89
Miguel Sampaio <sampaio.veiga@gmail.com>
2013-11-08 16:40:10 +0000

    refactor user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		@user.email.downcase!
+		@user.ativo = false
 
 		if @user.save
 			redirect_to log_in_path, notice: 'Utilizador registado com sucesso.'
